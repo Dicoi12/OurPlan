d58013c74c23ac3e985dce37c41da7cb1fd73c37
Darius <dicoidarius1202@gmail.com>
2025-09-30 22:09:15 +0300

    Add service result.

---
+++ OurPlan/Helpers/ServiceResult.cs
@@ -0,0 +1,9 @@
+﻿namespace OurPlan.Helpers
+{
+    public class ServiceResult<T>
+    {
+        public bool IsSuccesful { get; set; }
+        public List<string> ValidationMessage { get; set; } = new();
+        public T Result { get; set; }
+    }
+}
