57929a02c54d8383312f91a8d6edbcb99f971f40
Darius <dicoidarius1202@gmail.com>
2025-10-08 21:42:17 +0300

    task

--- OurPlan/DTO/GroupTokenModel.cs
+++ OurPlan/DTO/GroupTokenModel.cs
@@ -1,12 +1,13 @@
+using AutoMapper;
 using OurPlan.Entity;
 
 namespace OurPlan.DTO
 {
     public class GroupTokenModel
     {
-        
+
         public int Id { get; set; }
-       
+
         public int GroupId { get; set; }
         public string Token { get; set; } = string.Empty;
         public DateTime ExpiryDate { get; set; }
