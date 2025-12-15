e93ef9473ffb03207e8ef297b671de526f6130d1
Graf <grrobert2024@gmail.com>
2025-12-15 15:30:38 +0200

    GroupTask / model

---
+++ OurPlan/DTO/GroupTaskModel.cs
@@ -0,0 +1,31 @@
+using OurPlan.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace OurPlan.DTO
+{
+    public class GroupTaskModel
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        [StringLength(50)]
+        public string Title { get; set; }
+        
+        [StringLength(1000)]
+        
+        public string Description { get; set; } 
+        
+        public TaskPriority Priority { get; set; }
+        
+        public DateTime StartDate { get; set; }
+        
+        public string? DueDate { get; set; }
+        
+        public int GroupId { get; set; }
+        
+        public int CreatedByUserId { get; set; }
+        
+        
+    }
+}
---
+++ OurPlan/Entity/GroupTask.cs
@@ -0,0 +1,43 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OurPlan.Entity
+{
+    public enum TaskPriority
+    {
+        Low = 1,
+        Medium = 2,
+        High = 3
+    }
+    
+    public class GroupTask
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        [StringLength(50)]
+        public string Title { get; set; }
+        
+        [StringLength(1000)]
+        
+        public string Description { get; set; } 
+        
+        public TaskPriority Priority { get; set; }
+        
+        public DateTime StartDate { get; set; }
+        
+        public string? DueDate { get; set; }
+        
+        
+        [ForeignKey("GroupId")]
+        public int GroupId { get; set; }
+        
+        [ForeignKey("CreatedByUserId")]
+        public int CreatedByUserId { get; set; }
+        
+        
+        
+        
+    }
+}
