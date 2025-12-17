4db6f1d27a29ee55c66bc61be00b50ec6454bac3
Graf <grrobert2024@gmail.com>
2025-12-17 12:33:28 +0200

    GroupTaskService.cs

--- OurPlan/Services/Interfaces/IGroupTaskService.cs
+++ OurPlan/Services/Interfaces/IGroupTaskService.cs
@@ -6,12 +6,13 @@ namespace OurPlan.Services.Interfaces
     public interface IGroupTaskService
     {
     
-        public Task<ServiceResult<GroupModel>> CreateGroupTask(GroupTaskModel groupTask);
+        //public Task<ServiceResult<GroupTaskModel>> CreateGroupTask(GroupTaskModel groupTask);
         public Task<ServiceResult<List<GroupTaskModel>>> GetAllGroupTasks(int groupId);
         public Task<ServiceResult<GroupTaskModel>> GetGroupTask(int groupId, int taskId);
-        public Task<ServiceResult<List<GroupTaskModel>>> GetTasksCurrentDay(int groupId);
+        public Task<ServiceResult<List<GroupTaskModel>>> GetTasksForCurrentDay(int groupId);
+        public Task<ServiceResult<List<GroupTaskModel>>> GetValideTasks(int groupId);
         
-        public Task<ServiceResult<GroupTaskModel>> UpdateGroupTask(int groupId,int taskId, GroupTaskModel groupTask);
+        public Task<ServiceResult<GroupTaskModel>> UpdateGroupTask( GroupTaskModel groupTask);
         public Task<ServiceResult<bool>> DeleteGroupTask(int groupId, int taskId);
         
         
